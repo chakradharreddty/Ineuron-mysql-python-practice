a4d3279339b4d2b8dfaaea066d8e2c864ca08b06
vishnu chakradhar reddy <53368312+chakradharreddty@users.noreply.github.com>
2023-01-10 16:46:41 +0530

    mysql+python practice

--- Ex3.py
+++ Ex3.py
@@ -4,7 +4,7 @@ cur = con.cursor()
 # cur.execute("CREATE DATABASE employee")
 cur.execute("USE employee")
 # cur.execute("CREATE TABLE employeedet(emp_id INT(2) AUTO_INCREMENT,emp_name VARCHAR(30),city VARCHAR(20),age INT(2),PRIMARY KEY(emp_id))")
-# cur.execute("INSERT INTO employeedet(emp_name,city,age) VALUES('chakri','kakinada',23),('indhu','chennai',21)")
+# cur.execute("INSERT INTO employeedet(emp_name,city,age) VALUES('chakri','kakinada',23),('krish','chennai',21)")
 # con.commit()
 cur.execute("SELECT * FROM employeedet")
 for i in cur:
